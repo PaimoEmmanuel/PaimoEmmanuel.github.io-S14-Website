43e987fb8717c589231c50e6005394563b2b6b2f
Paimo Emmanuel <paimoemmanuel2016@gmail.com>
2020-02-12 15:39:33 +0100

    Wavy animation, last footer padding

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var headerText = document.getElementById('home-header-text');
+/*var headerText = document.getElementById('home-header-text');
 
 headerText.innerHTML = '';
 var n = 0;
@@ -21,8 +21,39 @@ var typeTimer = setInterval(function() {
       };
     }, 500);
   };
-}, 60)
+}, 60);*/
 
+//FOR THE MOVING WAVY VECTOR1 ON SCROLL
+var window_width = $(window).width() - $('#wavy').width();
+
+
+var document_height = $(document).height() + 700;
+
+$(function () {
+    $(window).scroll(function () {
+        var scroll_position = $(window).scrollTop() + 400;
+        var object_position_left = window_width * (scroll_position / document_height);
+        $('#wavy').css({
+            'top': object_position_left
+        });
+    });
+});
+
+//FOR THE MOVING WAVY VECTOR1 ON SCROLL
+var window_width2 = $(window).width() - $('#wavy2').width();
+
+
+var document_height2 = $(document).height() -1000 ;
+
+$(function () {
+    $(window).scroll(function () {
+        var scroll_position2 = $(window).scrollTop() ;
+        var object_position_left2 = window_width2 * (scroll_position2 / document_height2);
+        $('#wavy2').css({
+            'bottom': object_position_left2
+        });
+    });
+});
 //For the perform patnership visibility
 
 // FOr the build, UiUx tabs
