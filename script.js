e8e19ea911216f758ead4693eee93fc984f1f6e9
Paimo Emmanuel <paimoemmanuel2016@gmail.com>
2020-03-02 18:21:16 +0100

    Improved responsiveness

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
+
 //FOR THE TYPEWRITER EFFECT
-
-
 function typewriter(string){
 var headerText = document.getElementById('home-header-text');
 var n = 0;
@@ -26,11 +25,38 @@ var typeTimer = setInterval(function() {
 }, 80);
 };
 typewriter("We create innovative digital solutions");
+//BLINKING DOT ON CONTACT PAGE
+/*var blinker = $('#blinker')
+(function blink(){
+  var typeTimer = setInterval(function() {
+    //blinker.style.visibility = 'visible'? blinker.style.visibility = 'hidden':blinker.style.visibility = 'visible';
+    console.log(blinker.style.visibility)
+  }, 80);
+  })();*/
+//MOVING BACKGROUND WITH MOUSE DIRECTION
+//var moveA = 90;
 
+/*var moveA = $('.header').pageX;
+console.log(moveA)
+var moveB = $('.header').pageY;
+$('.header').mousemove(function(e){
+  //var moveX = (e.pageX * -1 / 15);
+  //var moveY = (e.pageY * -1 / 15);
+  //moveA++;
+  var moveY = (e.pageY/1000);
+  var moveX = (e.pageX/1000);
+  //var moveX = .2;
+
+  moveB = e.pageY/1000;
+  $('.homepage-heading-primary__bg').css('transform', `matrix(1, 0, ${0.0 + moveX}, 0, 0, 1.07, 0, 0, ${0.4 + moveY}, 0, ${0.3 + moveX}, 0, 0, -${2.0 + moveY}, 0, 1) rotate(-7.1deg)`)//moveX + 'px ' + moveY + 'px');
+  //console.log(moveX, moveY, 'transform', `matrix3d(1, 0, ${0.0 + moveX}, 0, 0, 1.07, 0, 0, ${0.4 + moveY}, 0, ${0.3 + moveX}, 0, 0, -${2.0 + moveY}, 0, 1) rotate(-7.1deg)`)
+  //console.log($('.homepage-heading-primary__bg'))
+  $('.wavy-a').css('margin-left', moveX + 'px');
+})*/
 
 //MOVING BACKGROUND WITH MOUSE DIRECTION
 var moveA = 90;
-$('.header').mousemove(function(e){
+$('.homepage-heading-primary').mousemove(function(e){
   var moveX = (e.pageX * -1 / 15);
   var moveY = (e.pageY * -1 / 15);
   moveA++;
@@ -38,6 +64,8 @@ $('.header').mousemove(function(e){
   $('.homepage-heading-primary__bg').css('background-position', moveX + 'px ' + moveY + 'px');
   $('.wavy-a').css('margin-left', moveX + 'px');
 })
+
+
 //FOR THE MOVING WAVY VECTOR1 ON SCROLL
 /*var window_width = $(window).width() - $('#wavy').width();
 var document_height = $(document).height() + 700;
@@ -50,6 +78,7 @@ $(function () {
         });
     });
 });*/
+
 
 //FOR THE MOVING WAVY VECTOR1 ON SCROLL
 var window_width2 = $(window).width() - $('#wavy2').width();
