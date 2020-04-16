c682c788c7353a9b399bc956a1476cfb6123824f
Paimo Emmanuel <paimoemmanuel2016@gmail.com>
2020-04-16 17:13:11 +0100

    About us tab

--- script.js
+++ script.js
@@ -25,34 +25,7 @@ var typeTimer = setInterval(function() {
 }, 80);
 };
 //typewriter("We create innovative digital solutions");
-//BLINKING DOT ON CONTACT PAGE
-/*var blinker = $('#blinker')
-(function blink(){
-  var typeTimer = setInterval(function() {
-    //blinker.style.visibility = 'visible'? blinker.style.visibility = 'hidden':blinker.style.visibility = 'visible';
-    console.log(blinker.style.visibility)
-  }, 80);
-  })();*/
-//MOVING BACKGROUND WITH MOUSE DIRECTION
-//var moveA = 90;
 
-/*var moveA = $('.header').pageX;
-console.log(moveA)
-var moveB = $('.header').pageY;
-$('.header').mousemove(function(e){
-  //var moveX = (e.pageX * -1 / 15);
-  //var moveY = (e.pageY * -1 / 15);
-  //moveA++;
-  var moveY = (e.pageY/1000);
-  var moveX = (e.pageX/1000);
-  //var moveX = .2;
-
-  moveB = e.pageY/1000;
-  $('.homepage-heading-primary__bg').css('transform', `matrix(1, 0, ${0.0 + moveX}, 0, 0, 1.07, 0, 0, ${0.4 + moveY}, 0, ${0.3 + moveX}, 0, 0, -${2.0 + moveY}, 0, 1) rotate(-7.1deg)`)//moveX + 'px ' + moveY + 'px');
-  //console.log(moveX, moveY, 'transform', `matrix3d(1, 0, ${0.0 + moveX}, 0, 0, 1.07, 0, 0, ${0.4 + moveY}, 0, ${0.3 + moveX}, 0, 0, -${2.0 + moveY}, 0, 1) rotate(-7.1deg)`)
-  //console.log($('.homepage-heading-primary__bg'))
-  $('.wavy-a').css('margin-left', moveX + 'px');
-})*/
 
 //MOVING BACKGROUND WITH MOUSE DIRECTION
 var moveA = 90;
@@ -67,33 +40,8 @@ $('.homepage-heading-primary').mousemove(function(e){
 })
 
 
-//FOR THE MOVING WAVY VECTOR1 ON SCROLL
-/*var window_width = $(window).width() - $('#wavy').width();
-var document_height = $(document).height() + 700;
-$(function () {
-    $(window).scroll(function () {
-        var scroll_position = $(window).scrollTop() + 400;
-        var object_position_left = window_width * (scroll_position / document_height);
-        $('#wavy').css({
-            'top': object_position_left
-        });
-    });
-});*/
-
 //
-window.onscroll = function() {myFunction()};
 
-var header = document.getElementsByClassName("section-work")[0];
-var sticky = header.offsetTop;
-//console.log(sticky);
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-    console.log(window.pageYOffset, sticky);
-  } else {
-    header.classList.remove("sticky");
-  }
-}
 
 //FOR THE MOVING WAVY VECTOR1 ON SCROLL
 var window_width2 = $(window).width() - $('#wavy2').width();
@@ -125,17 +73,63 @@ function nextFn(){
 /*$(document).ready(function(){
   myVar = setInterval("nextFn()", 1000);
 });*/
-next.onclick = nextFn;
-prev.onclick = function(){
-  srcArr.unshift(srcArr.pop());
-  for(let i =0; i < clientLogoPosition.length; i++){
-      clientLogoPosition[i].src = srcArr[i];
+// next.onclick = nextFn;
+// prev.onclick = function(){
+//   srcArr.unshift(srcArr.pop());
+//   for(let i =0; i < clientLogoPosition.length; i++){
+//       clientLogoPosition[i].src = srcArr[i];
+//   }
+// }
+
+$.fn.isInViewport = function() {
+  var elementTop = $(this).offset().top;
+  var elementBottom = elementTop + $(this).outerHeight() / 2.5;
+
+  var viewportTop = $(window).scrollTop();
+  var viewportBottom = viewportTop + $(window).height();
+
+  return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+//For about us-our expertise tab
+$(window).on('resize scroll', function() {
+  if ($(".sp1").isInViewport()) {
+      // do something
+      $('.p1').css('color', '#000000');
+  } else {
+      // do something else
+      $('.p1').css('color', '#BDBDBD');
   }
-}
+});
+//Tab 2 WEB
+$(window).on('resize scroll', function() {
+  if ($(".sp2").isInViewport()) {
+      // do something
+      $('.p2').css('color', '#000000');
+  } else {
+      // do something else
+      $('.p2').css('color', '#BDBDBD');
+  }
+});
 
+//Tab 3 - DESIGN
+$(window).on('resize scroll', function() {
+  if ($(".sp3").isInViewport()) {
+      // do something
+      $('.p3').css('color', '#000000');
+  } else {
+      // do something else
+      $('.p3').css('color', '#BDBDBD');
+  }
+});
 
-//FOR BLINKING DOT ON CONTACT PAGE
-
-
-// FOr the build, UiUx tabs
-
+// Tab 4 - UI/UX
+$(window).on('resize scroll', function() {
+  if ($(".sp4").isInViewport()) {
+      // do something
+      $('.p4').css('color', '#000000');
+  } else {
+      // do something else
+      $('.p4').css('color', '#BDBDBD');
+  }
+});
